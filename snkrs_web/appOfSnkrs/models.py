11f05e0b9d50f6557afb18f09c20bc75b9ceabc3
honel <546501664@qq.com>
2018-11-06 18:57:35 +0800

    fix web

--- snkrs_web/appOfSnkrs/models.py
+++ snkrs_web/appOfSnkrs/models.py
@@ -6,10 +6,7 @@ from django.db import models
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
     phone = models.CharField(max_length=11, null=False, unique=True)
-    password = models.CharField(max_length=64)
-    addr = models.CharField(max_length=128)
-    email = models.CharField(max_length=64)
-    size = models.CharField(max_length=32, null=True)
+    password = models.CharField(max_length=64, null=False)
 
     def __str__(self):
         return "<Account Object: {}>".format(self.phone)
--- snkrs_web/appOfSnkrs/views.py
+++ snkrs_web/appOfSnkrs/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, HttpResponse, redirect
 from appOfSnkrs import models
-
 # Create your views here.
 
 # 账号列表
 def account_list(request):
     ret = models.Account.objects.all().order_by("id")
+    if request.method == "POST":
+        phone = request.POST.get("phone", None)
+        password = request.POST.get("password", None)
+        if len(phone) != 11:
+            message = "手机号码位数错误"
+            print("错误手机号")
+        return render(request, "account_list.html", {"account_list": ret, "message":message})
     return render(request, "account_list.html", {"account_list": ret})
 
 
-# 添加账号
-def add_account(request):
-    error_msg = ""
-    if request.method == "POST":
-        new_name = request.POST.get("account_name", None)
-        if new_name:
-            models.Account.objects.create(name=new_name)
-            return redirect("/account_list/")
-        else:
-            error_msg = "出版社名字不能为空!"
-    return render(request, "add_account.html", {"error": error_msg})
+# # 添加账号
+# def add_account(request):
+#     error_msg = ""
+#     if request.method == "POST":
+#         phone = request.POST.get("phone", None)
+#         password = request.POST.get("password", None)
+#         print(phone)
+#         print(password)
+#         # if new_name:
+#         #     models.Account.objects.create(name=new_name)
+#         #     return redirect("/account_list/")
+#         # else:
+#         #     error_msg = "出版社名字不能为空!"
+#     return render(request, "account_list.html", {"error": error_msg})
